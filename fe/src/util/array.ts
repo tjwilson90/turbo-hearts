e7ae070ab85e6ad349a8e3c61f21f7320d1c8295
Dan Cervelli <dan@closedcontour.com>
2020-02-15 12:32:51 -0800

    Refactor.

---
+++ fe/src/util/array.ts
@@ -0,0 +1,15 @@
+export function removeAll<T>(arr: T[], toRemove: T[]) {
+  const removeSet = new Set(toRemove);
+  for (let i = 0; i < arr.length; i++) {
+    if (removeSet.has(arr[i])) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+export function pushAll<T>(arr: T[], toAdd: T[]) {
+  for (const add of toAdd) {
+    arr.push(add);
+  }
+}
