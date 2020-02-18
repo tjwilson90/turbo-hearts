f1f4a079692129996a3ca17a379d8fb4968a698c
Tim Wilson <twilson@palantir.com>
2020-02-17 19:48:24 -0800

    duck bot

---
+++ src/bot/duck.rs
@@ -0,0 +1,66 @@
+use crate::{
+    bot::{Algorithm, BotState},
+    cards::{Card, Cards},
+};
+use rand::Rng;
+
+pub struct Duck;
+
+impl Duck {
+    pub const NAME: &'static str = "duck";
+
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+impl Algorithm for Duck {
+    fn pass(&mut self, state: &BotState) -> Cards {
+        let mut pass = Cards::NONE;
+        let mut hand = state.pre_pass_hand;
+        for _ in 0..3 {
+            let worst_card = hand
+                .into_iter()
+                .max_by_key(|card| score(*card, hand))
+                .unwrap();
+            pass |= worst_card;
+            hand -= worst_card;
+        }
+        pass
+    }
+
+    fn charge(&mut self, _: &BotState) -> Cards {
+        Cards::NONE
+    }
+
+    fn play(&mut self, state: &BotState) -> Card {
+        let cards = state.game.legal_plays(state.post_pass_hand);
+        if state.game.current_trick.is_empty() {
+            let index = rand::thread_rng().gen_range(0, cards.len());
+            return cards.into_iter().nth(index).unwrap();
+        }
+        let suit = state.game.current_trick[0].suit();
+        if !suit.cards().contains_any(cards) {
+            return cards
+                .into_iter()
+                .max_by_key(|card| score(*card, state.post_pass_hand - state.game.played))
+                .unwrap();
+        }
+        let winner = state
+            .game
+            .current_trick
+            .iter()
+            .filter(|card| card.suit() == suit)
+            .max()
+            .unwrap();
+        let duck = cards.into_iter().filter(|card| card < winner).max();
+        match duck {
+            Some(card) => card,
+            _ => cards.into_iter().max().unwrap(),
+        }
+    }
+}
+
+fn score(card: Card, hand: Cards) -> usize {
+    14 * card.rank() as usize + 13 - (card.suit().cards() & hand).len()
+}
