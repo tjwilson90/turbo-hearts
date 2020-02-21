ab1c195e0f0fe0884cfbe6111e1eba82c266d43d
Dan Cervelli <dan@closedcontour.com>
2020-02-20 20:19:33 -0800

    Standard animations.

--- packages/game/src/events/YourChargeEvent.ts
+++ packages/game/src/events/YourChargeEvent.ts
@@ -1,76 +1,38 @@
-import TWEEN from "@tweenjs/tween.js";
 import { TurboHearts } from "../game/TurboHearts";
-import { Event, SpriteCard, YourChargeEventData } from "../types";
+import { Event, YourChargeEventData } from "../types";
+import { Button } from "../ui/Button";
+import { CardPickSupport } from "./animations/CardPickSupport";
 import { spriteCardsOf } from "./helpers";
 import { getPlayerAccessor } from "./playerAccessors";
-import { Button } from "../ui/Button";
-import { FASTER_ANIMATION_DURATION, BOTTOM, CARD_DISPLAY_HEIGHT } from "../const";
 
 export class YourChargeEvent implements Event {
   public type = "your_charge" as const;
 
+  private cardPickSupport: CardPickSupport;
   private button: Button;
-  private finished = false;
-  private chargeableCards: SpriteCard[] = [];
-  private cardsToCharge: Set<SpriteCard> = new Set();
-  private cardMap: Map<PIXI.Sprite, SpriteCard> = new Map();
-  private cardTweens: Map<PIXI.Sprite, TWEEN.Tween> = new Map();
 
   constructor(private th: TurboHearts, private event: YourChargeEventData) {}
 
   public begin() {
     const player = getPlayerAccessor(this.th.bottomSeat, this.th.bottomSeat)(this.th);
-    this.chargeableCards = spriteCardsOf(player.cards, ["TC", "JD", "AH", "QS"]);
-    for (const card of this.chargeableCards) {
-      card.sprite.interactive = true;
-      this.cardMap.set(card.sprite, card);
-      card.sprite.addListener("pointertap", this.onClick);
-      card.sprite.buttonMode = true;
-    }
+    const chargeableCards = spriteCardsOf(player.cards, ["TC", "JD", "AH", "QS"]);
+    this.cardPickSupport = new CardPickSupport(chargeableCards);
     this.button = new Button("Charge Cards", this.submitCharge);
     this.button.setEnabled(true);
     this.th.app.stage.addChild(this.button.container);
     this.th.asyncEvent = this;
-    this.finished = true;
-    // TODO fix for non-classic
   }
-
-  private tweenTo(sprite: PIXI.Sprite, y: number) {
-    const existingTween = this.cardTweens.get(sprite);
-    if (existingTween !== undefined) {
-      existingTween.stop();
-    }
-    const tween = new TWEEN.Tween(sprite.position).to({ y }, FASTER_ANIMATION_DURATION);
-    this.cardTweens.set(sprite, tween);
-    tween.start();
-  }
-
-  private onClick = (event: PIXI.interaction.InteractionEvent) => {
-    const card = this.cardMap.get(event.currentTarget as PIXI.Sprite);
-    if (card !== undefined) {
-      if (this.cardsToCharge.has(card)) {
-        this.cardsToCharge.delete(card);
-        this.tweenTo(card.sprite, BOTTOM.y);
-      } else {
-        this.cardsToCharge.add(card);
-        this.tweenTo(card.sprite, BOTTOM.y - CARD_DISPLAY_HEIGHT / 4);
-      }
-    }
-  };
 
   private submitCharge = () => {
     this.button.setEnabled(false);
+    this.cardPickSupport.cleanUp();
     this.th.app.stage.removeChild(this.button.container);
-    for (const card of this.chargeableCards) {
-      card.sprite.interactive = false;
-      card.sprite.removeListener("pointertap", this.onClick);
-      card.sprite.buttonMode = false;
-    }
     this.th.asyncEvent = undefined;
-    this.th.submitter.chargeCards([...this.cardsToCharge.values()].map(c => c.card));
+    this.th.submitter.chargeCards([...this.cardPickSupport.picked.values()].map(c => c.card));
   };
 
   public isFinished() {
-    return this.finished;
+    // Async (at least classic)
+    return true;
   }
 }
