58945dcad70e40a6a1777b4856e64c2b4e96f254
Dan Cervelli <dan@closedcontour.com>
2020-02-20 20:19:33 -0800

    Standard animations.

--- packages/game/src/events/StartPassingEvent.ts
+++ packages/game/src/events/StartPassingEvent.ts
@@ -1,81 +1,45 @@
-import TWEEN from "@tweenjs/tween.js";
-import * as PIXI from "pixi.js";
-import { BOTTOM, CARD_DISPLAY_HEIGHT, FASTER_ANIMATION_DURATION } from "../const";
 import { TurboHearts } from "../game/TurboHearts";
-import { Event, SpriteCard, StartPassingEventData, Pass } from "../types";
+import { Event, Pass, StartPassingEventData } from "../types";
 import { Button } from "../ui/Button";
+import { CardPickSupport } from "./animations/CardPickSupport";
 import { getPlayerAccessor } from "./playerAccessors";
 
-const directionText: {[P in Pass]: string} = {
+const directionText: { [P in Pass]: string } = {
   left: "Left",
   right: "Right",
   across: "Across",
-  keeper: "In",
+  keeper: "In"
 };
 
 export class StartPassingEvent implements Event {
   public type = "start_passing" as const;
 
+  private cardPickSupport: CardPickSupport;
   private button: Button;
-  private finished = false;
-  private cardsToPass: Set<SpriteCard> = new Set();
-  private cardMap: Map<PIXI.Sprite, SpriteCard> = new Map();
-  private cardTweens: Map<PIXI.Sprite, TWEEN.Tween> = new Map();
 
   constructor(private th: TurboHearts, private event: StartPassingEventData) {}
 
   public begin() {
     const player = getPlayerAccessor(this.th.bottomSeat, this.th.bottomSeat)(this.th);
-    for (const card of player.cards) {
-      card.sprite.interactive = true;
-      this.cardMap.set(card.sprite, card);
-      card.sprite.addListener("pointertap", this.onClick);
-      card.sprite.buttonMode = true;
-    }
+    this.cardPickSupport = new CardPickSupport(player.cards, this.setButtonEnabled);
     this.button = new Button("Pass 3 Cards " + directionText[this.th.pass], this.submitPass);
+    this.setButtonEnabled();
     this.th.app.stage.addChild(this.button.container);
-    this.button.setEnabled(this.cardsToPass.size === 3);
-    // Passing is non-blocking.
-    this.finished = true;
   }
 
-  private tweenTo(sprite: PIXI.Sprite, y: number) {
-    const existingTween = this.cardTweens.get(sprite);
-    if (existingTween !== undefined) {
-      existingTween.stop();
-    }
-    const tween = new TWEEN.Tween(sprite.position).to({ y }, FASTER_ANIMATION_DURATION);
-    this.cardTweens.set(sprite, tween);
-    tween.start();
-  }
-
-  private onClick = (event: PIXI.interaction.InteractionEvent) => {
-    const card = this.cardMap.get(event.currentTarget as PIXI.Sprite);
-    if (card !== undefined) {
-      if (this.cardsToPass.has(card)) {
-        this.cardsToPass.delete(card);
-        this.tweenTo(card.sprite, BOTTOM.y);
-      } else {
-        this.cardsToPass.add(card);
-        this.tweenTo(card.sprite, BOTTOM.y - CARD_DISPLAY_HEIGHT / 4);
-      }
-    }
-    this.button.setEnabled(this.cardsToPass.size === 3);
+  private setButtonEnabled = () => {
+    this.button.setEnabled(this.cardPickSupport.picked.size === 3);
   };
 
   private submitPass = () => {
-    const player = getPlayerAccessor(this.th.bottomSeat, this.th.bottomSeat)(this.th);
     this.button.setEnabled(false);
+    this.cardPickSupport.cleanUp();
     this.th.app.stage.removeChild(this.button.container);
-    for (const card of player.cards) {
-      card.sprite.interactive = false;
-      card.sprite.removeListener("pointertap", this.onClick);
-      card.sprite.buttonMode = false;
-    }
-    this.th.submitter.passCards([...this.cardsToPass.values()].map(c => c.card));
+    this.th.submitter.passCards([...this.cardPickSupport.picked.values()].map(c => c.card));
   };
 
   public isFinished() {
-    return this.finished;
+    // Passing is non-blocking.
+    return true;
   }
 }
--- packages/game/src/events/YourPlayEvent.ts
+++ packages/game/src/events/YourPlayEvent.ts
@@ -1,83 +1,32 @@
-import TWEEN from "@tweenjs/tween.js";
-import * as PIXI from "pixi.js";
-import { BOTTOM, CARD_DISPLAY_HEIGHT, FASTER_ANIMATION_DURATION } from "../const";
-import { Player, TurboHearts } from "../game/TurboHearts";
-import { Card, Event, SpriteCard, YourPlayEventData } from "../types";
+import { TurboHearts } from "../game/TurboHearts";
+import { Event, YourPlayEventData } from "../types";
+import { CardPickSupport } from "./animations/CardPickSupport";
 import { spriteCardsOf } from "./helpers";
 import { getPlayerAccessor } from "./playerAccessors";
-
-function isCharged(player: Player, card: Card) {
-  return player.chargedCards.some(c => c.card === card);
-}
 
 export class YourPlayEvent implements Event {
   public type = "your_play" as const;
 
   private finished = false;
-  private playableCards: SpriteCard[];
-  private legalCards: Set<SpriteCard>;
-  private cardMap: Map<PIXI.Sprite, SpriteCard> = new Map();
-  private cardTweens: Map<PIXI.Sprite, TWEEN.Tween> = new Map();
-  private player: Player;
+  private cardPickSupport: CardPickSupport;
 
   constructor(private th: TurboHearts, private event: YourPlayEventData) {}
 
   public begin() {
-    this.player = getPlayerAccessor(this.th.bottomSeat, this.th.bottomSeat)(this.th);
-    this.playableCards = [...this.player.cards, ...this.player.chargedCards];
-    this.legalCards = new Set(spriteCardsOf(this.playableCards, this.event.legalPlays));
-    for (const card of this.playableCards) {
-      if (this.legalCards.has(card)) {
-        this.cardMap.set(card.sprite, card);
-        card.sprite.interactive = true;
-        card.sprite.addListener("pointertap", this.onClick);
-        card.sprite.addListener("pointerover", this.onOver);
-        card.sprite.addListener("pointerout", this.onOut);
-        card.sprite.buttonMode = true;
-      }
-    }
+    const player = getPlayerAccessor(this.th.bottomSeat, this.th.bottomSeat)(this.th);
+    const playableCards = [...player.cards, ...player.chargedCards];
+    const legalPlays = spriteCardsOf(playableCards, this.event.legalPlays);
+    this.cardPickSupport = new CardPickSupport(legalPlays, this.onPick);
   }
 
-  private onClick = (event: PIXI.interaction.InteractionEvent) => {
-    const card = this.cardMap.get(event.currentTarget as PIXI.Sprite);
-    if (card !== undefined) {
-      for (const card of this.playableCards) {
-        if (this.legalCards.has(card)) {
-          card.sprite.interactive = false;
-          card.sprite.removeListener("pointertap", this.onClick);
-          card.sprite.removeListener("pointerover", this.onOver);
-          card.sprite.removeListener("pointerout", this.onOut);
-          card.sprite.buttonMode = false;
-        }
-      }
-      this.th.submitter.playCard(card.card).then(() => {
-        this.finished = true;
-      });
+  private onPick = async () => {
+    const cards = Array.from(this.cardPickSupport.picked.values());
+    if (cards.length !== 1) {
+      return;
     }
-  };
-
-  private tweenTo(sprite: PIXI.Sprite, y: number) {
-    const existingTween = this.cardTweens.get(sprite);
-    if (existingTween !== undefined) {
-      existingTween.stop();
-    }
-    const tween = new TWEEN.Tween(sprite.position).to({ y }, FASTER_ANIMATION_DURATION);
-    this.cardTweens.set(sprite, tween);
-    tween.start();
-  }
-
-  private onOver = (event: PIXI.interaction.InteractionEvent) => {
-    const sprite = event.currentTarget as PIXI.Sprite;
-    const card = this.cardMap.get(sprite);
-    const bottom = isCharged(this.player, card.card) ? BOTTOM.chargeY : BOTTOM.y;
-    this.tweenTo(sprite, bottom - CARD_DISPLAY_HEIGHT / 4);
-  };
-
-  private onOut = (event: PIXI.interaction.InteractionEvent) => {
-    const sprite = event.currentTarget as PIXI.Sprite;
-    const card = this.cardMap.get(sprite);
-    const bottom = isCharged(this.player, card.card) ? BOTTOM.chargeY : BOTTOM.y;
-    this.tweenTo(sprite, bottom);
+    this.cardPickSupport.cleanUp();
+    await this.th.submitter.playCard(cards[0].card);
+    this.finished = true;
   };
 
   public isFinished() {
