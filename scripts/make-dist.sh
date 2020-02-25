4cd512fb2970d5719df2229bc3cdcf6ae611cc82
Tim Wilson <twilson@palantir.com>
2020-02-24 21:13:37 -0800

    try publishing a dist with github actions

---
+++ scripts/make-dist.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p turbo-hearts/assets/dist/assets
+cp target/release/turbo-hearts turbo-hearts
+cp -r assets/* turbo-hearts/assets
+cp assets/*.jpg turbo-hearts/assets/dist/assets
+cp -r assets/cards turbo-hearts/assets/dist/assets
+cp packages/game/node_modules/normalize.css/normalize.css turbo-hearts/assets/dist/assets
+tar czvf turbo-hearts.tgz turbo-hearts/
+rm -rf turbo-hearts
