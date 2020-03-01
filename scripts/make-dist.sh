9e301e769cd4b8f6af50eb7390fac4e207f1feda
Tim Wilson <twilson@palantir.com>
2020-02-29 21:41:19 -0800

    fix up make-dist script

--- scripts/make-dist.sh
+++ scripts/make-dist.sh
@@ -2,8 +2,7 @@
 
 mkdir -p turbo-hearts/assets/{auth,lobby,game}
 cp target/x86_64-unknown-linux-musl/release/turbo-hearts turbo-hearts
-cp auth.html turbo-hearts/assets/auth/index.html
-cp lobby.html turbo-hearts/assets/lobby/index.html
 cp packages/game/dist/* turbo-hearts/assets/game
+cp -r assets turbo-hearts
 tar czvf turbo-hearts.tgz turbo-hearts/
 rm -rf turbo-hearts
