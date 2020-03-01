ae54a77573c72819d5ad31b9cd384fa0a5fa7de9
Tim Slatcher <tslatcher@palantir.com>
2020-03-01 14:59:25 -0700

    fix build tasks

--- scripts/make-dist.sh
+++ scripts/make-dist.sh
@@ -2,7 +2,6 @@
 
 mkdir -p turbo-hearts/assets/{auth,lobby,game}
 cp target/x86_64-unknown-linux-musl/release/turbo-hearts turbo-hearts
-cp packages/game/dist/* turbo-hearts/assets/game
 cp -r assets turbo-hearts
 tar czvf turbo-hearts.tgz turbo-hearts/
 rm -rf turbo-hearts
