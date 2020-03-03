b4c4babb093b445f213623ad79953be23673d88e
Tim Slatcher <tslatcher@palantir.com>
2020-03-03 09:12:46 -0700

    wip

--- packages/lobby/src/index.tsx
+++ packages/lobby/src/index.tsx
@@ -1,9 +1,23 @@
+if (typeof process === "undefined") {
+  if (typeof window === "undefined") {
+    throw new Error("Ooops ...");
+  }
+  (window as any).process = { env: {} };
+}
+
+
 import { TurboHeartsLobbyEventSource } from "./TurboHeartsLobbyEventSource";
 import { LobbySnapshot, LobbySnapshotter } from "./lobbySnapshotter";
 import { TurboHeartsLobbyService } from "./TurboHeartsLobbyService";
+import * as ReactDOM  from "react-dom";
+import * as React from "react";
+import { Lobby } from "./components/Lobby";
 
 function render(snapshot: LobbySnapshot, service: TurboHeartsLobbyService) {
-  console.log(snapshot);
+  ReactDOM.render(
+      <Lobby {...snapshot} messages={[]} service={service}/>,
+      document.getElementById("lobby")
+  )
 }
 
 document.addEventListener("DOMContentLoaded", () => {
