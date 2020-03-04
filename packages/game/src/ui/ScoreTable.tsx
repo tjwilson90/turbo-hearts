cdafce761d04451f30dcc0078d7daf9302f3ccd6
Brian Lee <blee@palantir.com>
2020-03-03 20:46:29 -0800

    Add initials to scoreboard.

--- packages/game/src/ui/ScoreTable.tsx
+++ packages/game/src/ui/ScoreTable.tsx
@@ -1,9 +1,16 @@
 import * as React from "react";
 import { connect } from "react-redux";
-import { GameAppState } from "../state/types";
+import { GameAppState, User } from "../state/types";
+import { Seat } from "../types";
+import { POSITION_FOR_BOTTOM_SEAT } from "../util/seatPositions";
 
 export namespace ScoreTable {
   export interface StoreProps {
+    bottomSeat: Seat;
+    top: User | undefined;
+    right: User | undefined;
+    bottom: User | undefined;
+    left: User | undefined;
     scores: number[][]
   }
 
@@ -12,19 +19,29 @@ export namespace ScoreTable {
 
 class ScoreTableInternal extends React.Component<ScoreTable.Props> {
   public render() {
-    if (this.props.scores.length === 0) {
-      return <div className="score-table"></div>;
+    if (this.props.bottomSeat === undefined) {
+      return <div className="score-table"></div>
     }
     return <div className="score-table">
-      <tr><th>N</th><th>E</th><th>S</th><th>W</th></tr>
+      <tr>{POSITION_FOR_BOTTOM_SEAT[this.props.bottomSeat].map(position => <th title={this.props[position]?.name ?? "loading..."}>{nameToInitials(this.props[position])}</th>)}</tr>
       {this.props.scores.map(scoreRow => <tr>{scoreRow.map(score => <td>{score}</td>)}</tr>)}
     </div>;
   }
 }
 
+function nameToInitials(user: User | undefined) {
+  if (user === undefined) {
+    return "...";
+  }
+  if (user.name.startsWith("Bot (")) {
+    return user.userId.substr(0, 2);
+  }
+  return user.name.split(" ", 2).map(s => s[0]).join("");
+}
+
 function mapStateToProps(state: GameAppState): ScoreTable.StoreProps {
   return {
-    scores: state.game.scores
+    ...state.game,
   };
 }
 
