a46e75d1b7f76964335d87c66f18ccc437347226
Tim Slatcher <tslatcher@palantir.com>
2020-03-07 10:10:06 -0700

    pax -> rollup

---
+++ packages/game/rollup.config.js
@@ -0,0 +1,26 @@
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
+
+module.exports = {
+    input: 'dist/js/index.js',
+    output: {
+        file: '../../assets/game/index.js',
+        format: 'iife'
+    },
+    plugins: [
+        replace({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || "development")
+        }),
+        commonjs({
+            namedExports: {
+                'node_modules/react/index.js': ['Component', 'PureComponent', 'Fragment', 'Children', 'createElement', 'useMemo', 'useEffect', 'useLayoutEffect', "useContext", "useRef", ,"useReducer"],
+                'node_modules/react-is/index.js': ['isValidElementType', 'isContextConsumer'],
+                'node_modules/react-dom/index.js': ['render', 'unstable_batchedUpdates']
+            },
+        }),
+        nodeResolve({
+            preferBuiltins: false,
+        })
+    ],
+};
