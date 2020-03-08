c6cb9bf3408ef1f55b099916a95aba1487603308
Dan Cervelli <dan@closedcontour.com>
2020-03-07 23:48:17 -0800

    WIP.

--- packages/lobby/src/TurboHeartsLobbyService.ts
+++ packages/lobby/src/TurboHeartsLobbyService.ts
@@ -1,47 +1,57 @@
 import { BotStrategy, Rules } from "./types";
 
 export class TurboHeartsLobbyService {
-    private requestWithBody(body: any): RequestInit {
-        return {
-            credentials: "include",
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(body)
-        };
-    }
-
-    public createLobby(rules: Rules) {
-        return fetch(`/lobby/new`, this.requestWithBody({ rules }));
-    }
-
-    public leaveLobby(gameId: string) {
-        return fetch(`/lobby/leave`, this.requestWithBody({ game_id: gameId }));
-    }
-
-    public joinLobby(gameId: string, rules: Rules) {
-        return fetch(`/lobby/join`, this.requestWithBody({ game_id: gameId, rules }));
-    }
-
-    public addBot(gameId: string, rules: Rules, strategy: BotStrategy) {
-        return fetch(`/lobby/add_bot`, this.requestWithBody({ game_id: gameId, rules, strategy }));
-    }
-
-    public startGame(gameId: string) {
-        return fetch(`/lobby/start`, this.requestWithBody({ game_id: gameId }));
-    }
-
-    public chat(message: string) {
-        return fetch(`/lobby/chat`, this.requestWithBody({ message }));
-    }
-
-    public getUser = async (userId: string) => {
-        const resp = await fetch(`/users`, this.requestWithBody({ ids: [userId] }));
-        const json = (await resp.json()) as { name: string; id: string }[];
-        for (const item of json) {
-            return item.name;
-        }
-        return "Unknown";
+  private requestWithBody(body: any): RequestInit {
+    return {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
     };
+  }
+
+  public createLobby(rules: Rules) {
+    return fetch(`/lobby/new`, this.requestWithBody({ rules }));
+  }
+
+  public leaveLobby(gameId: string) {
+    return fetch(`/lobby/leave`, this.requestWithBody({ game_id: gameId }));
+  }
+
+  public joinLobby(gameId: string, rules: Rules) {
+    return fetch(`/lobby/join`, this.requestWithBody({ game_id: gameId, rules }));
+  }
+
+  public addBot(gameId: string, rules: Rules, strategy: BotStrategy) {
+    return fetch(`/lobby/add_bot`, this.requestWithBody({ game_id: gameId, rules, strategy }));
+  }
+
+  public startGame(gameId: string) {
+    return fetch(`/lobby/start`, this.requestWithBody({ game_id: gameId }));
+  }
+
+  public chat(message: string) {
+    return fetch(`/lobby/chat`, this.requestWithBody({ message }));
+  }
+
+  public getRecentGames() {
+    return fetch(`/scores`, {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  }
+
+  public getUser = async (userId: string) => {
+    const resp = await fetch(`/users`, this.requestWithBody({ ids: [userId] }));
+    const json = (await resp.json()) as { name: string; id: string }[];
+    for (const item of json) {
+      return item.name;
+    }
+    return "Unknown";
+  };
 }
---
+++ packages/lobby/src/utils/gameResults.ts
@@ -0,0 +1,103 @@
+import { GameResult } from "../types";
+
+export interface ScoreDelta {
+    score: number;
+    delta: number;
+}
+
+export type HandScores = { [key: string]: ScoreDelta };
+
+export interface GameScores {
+    userIds: string[];
+    totals: HandScores;
+    scores: HandScores[];
+}
+
+export interface LeaderboardEntry {
+    userId: string;
+    games: number;
+    points: number;
+}
+
+export function calculateScores(game: GameResult): GameScores {
+    const handScores = [];
+    for (const hand of game.hands) {
+        const chargeSet = new Set(hand.charges);
+        const qsCharged = chargeSet.has("QS");
+        const jdCharged = chargeSet.has("JD");
+        const tcCharged = chargeSet.has("TC");
+        const ahCharged = chargeSet.has("AH");
+        const handResult: HandScores = {};
+        const scores = [];
+        for (let i = 0; i < 4; i++) {
+            const player = game.players[i];
+            const hearts = hand.hearts[i];
+            let score = hearts * (ahCharged ? 2 : 1);
+            const qs = hand.qsWinnerId === player.userId;
+            if (qs) {
+                score += 13 * (qsCharged ? 2 : 1);
+            }
+            if (hearts === 13 && qs) {
+                score *= -1;
+            }
+            const jd = hand.jdWinnerId === player.userId;
+            if (jd) {
+                score += -10 * (jdCharged ? 2 : 1);
+            }
+            const tc = hand.tcWinnerId === player.userId;
+            if (tc) {
+                score *= tcCharged ? 4 : 2;
+            }
+            scores.push(score);
+            handResult[player.userId] = { score, delta: 0 };
+        }
+        const deltas = scoresToDelta(scores);
+        for (let i = 0; i < 4; i++) {
+            const player = game.players[i];
+            handResult[player.userId].delta = deltas[i];
+        }
+        handScores.push(handResult);
+    }
+    const totals: { [key: string]: ScoreDelta } = {};
+    for (let i = 0; i < 4; i++) {
+        const player = game.players[i];
+        totals[player.userId] = { score: 0, delta: 0 };
+    }
+    for (const hand of handScores) {
+        for (let i = 0; i < 4; i++) {
+            const player = game.players[i];
+            totals[player.userId].score += hand[player.userId].score;
+            totals[player.userId].delta += hand[player.userId].delta;
+        }
+    }
+    return { totals, scores: handScores, userIds: game.players.map(p => p.userId) };
+}
+
+export function calculateLeaderboard(scores: GameScores[]) {
+    const leaderboard = new Map<string, LeaderboardEntry>();
+    for (const score of scores) {
+        for (const userId of score.userIds) {
+            if (!leaderboard.has(userId)) {
+                leaderboard.set(userId, {
+                    userId,
+                    games: 0,
+                    points: 0
+                });
+            }
+            const totals = score.totals[userId];
+            const entry = leaderboard.get(userId)!;
+            entry.games++;
+            entry.points += totals.delta;
+        }
+    }
+    return Array.from(leaderboard.values());
+}
+
+export function scoresToDelta(scores: number[]): number[] {
+    return [
+        scores[1] + scores[2] + scores[3] - 3 * scores[0],
+        scores[0] + scores[2] + scores[3] - 3 * scores[1],
+        scores[0] + scores[1] + scores[3] - 3 * scores[2],
+        scores[0] + scores[1] + scores[2] - 3 * scores[3]
+    ];
+}
