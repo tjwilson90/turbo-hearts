2bcc66eada9e85ee4a2e1684abf39c826488e803
Tim Wilson <twilson@palantir.com>
2020-03-08 09:23:21 -0700

    fix scores bugs

--- src/scores.rs
+++ src/scores.rs
@@ -8,12 +8,51 @@ use crate::{
 };
 use rusqlite::{Rows, NO_PARAMS};
 use serde::{Deserialize, Serialize};
+use std::mem;
 use warp::{Filter, Rejection, Reply};
 
-#[derive(Debug, Serialize)]
-struct CompleteGames {
-    games: Vec<CompleteGame>,
-}
+const INITIAL_SCORES: &'static str = r#"
+WITH ids AS
+(
+         SELECT   e.game_id
+         FROM     event e,
+                  game g
+         WHERE    e.game_id = g.game_id
+         AND      e.event_id = 0
+         AND      g.completed_time IS NOT NULL
+         AND      NOT e.event LIKE '%"type":"bot"%'
+         ORDER BY g.completed_time DESC limit 100)
+SELECT   game_id,
+         timestamp,
+         event
+FROM     event
+WHERE    game_id IN ids
+ORDER BY game_id,
+         event_id"#;
+
+const PAGED_SCORES: &'static str = r#"
+WITH ids AS
+(
+         SELECT   e.game_id
+         FROM     event e,
+                  game g
+         WHERE    e.game_id = g.game_id
+         AND      e.event_id = 0
+         AND      g.completed_time IS NOT NULL
+         AND      g.completed_time <
+                  (
+                         SELECT completed_time
+                         FROM   game
+                         WHERE  game_id = ?)
+         AND      NOT e.event LIKE '%"type":"bot"%'
+         ORDER BY g.completed_time DESC limit 100)
+SELECT   game_id,
+         timestamp,
+         event
+FROM     event
+WHERE    game_id IN ids
+ORDER BY game_id,
+         event_id"#;
 
 #[derive(Debug, Serialize)]
 struct CompleteGame {
@@ -41,76 +80,55 @@ pub fn scores(db: infallible!(Database)) -> reply!() {
     async fn handle(db: Database, request: Request) -> Result<impl Reply, Rejection> {
         let Request { game_id } = request;
         let games = db.run_read_only(|tx| {
-            let game_ids = match game_id {
+            Ok(match game_id {
                 None => {
-                    let mut stmt = tx.prepare(
-                        r#"
-                        SELECT   e.game_id
-                        FROM     event e,
-                                 game g,
-                                 game_player gp
-                        WHERE    e.game_id = g.game_id
-                        AND      e.game_id = gp.game_id
-                        AND      e.event_id = 0
-                        AND      g.completed_time IS NOT NULL
-                        AND      NOT e.event LIKE '%"type":"bot"'
-                        ORDER BY g.completed_time DESC limit 100"#,
-                    )?;
+                    let mut stmt = tx.prepare(INITIAL_SCORES)?;
                     let rows = stmt.query(NO_PARAMS)?;
-                    read_game_ids(rows)?
+                    read_games(rows)?
                 }
                 Some(game_id) => {
-                    let mut stmt = tx.prepare(
-                        r#"
-                        SELECT e.game_id
-                        FROM   event e,
-                               game g,
-                               game_player gp
-                        WHERE  e.game_id = g.game_id
-                               AND e.game_id = gp.game_id
-                               AND e.event_id = 0
-                               AND g.completed_time IS NOT NULL
-                               AND g.completed_time < (SELECT completed_time
-                                                       FROM   game
-                                                       WHERE  game_id = ?)
-                               AND NOT e.event LIKE '%"type":"bot"'
-                        ORDER  BY g.completed_time DESC
-                        LIMIT  100"#,
-                    )?;
+                    let mut stmt = tx.prepare(PAGED_SCORES)?;
                     let rows = stmt.query(&[game_id])?;
-                    read_game_ids(rows)?
+                    read_games(rows)?
                 }
-            };
-            let mut games = Vec::with_capacity(game_ids.len());
-            for game_id in game_ids {
-                let mut stmt = tx.prepare_cached(
-                    "SELECT timestamp, event FROM event WHERE game_id = ? ORDER BY event_id",
-                )?;
-                let mut rows = stmt.query(&[game_id])?;
-                let mut hands = Vec::with_capacity(4);
-                let mut state = GameState::new();
-                let mut timestamp = 0;
-                while let Some(row) = rows.next()? {
-                    timestamp = row.get(0)?;
-                    let event = row.get(1)?;
-                    let was_playing = state.phase.is_playing();
-                    state.apply(&event);
-                    let is_playing = state.phase.is_playing();
-                    if was_playing && !is_playing {
-                        hands.push(CompleteHand {
-                            charges: state.charged,
-                            hearts_won: [
-                                (state.won[0] & Cards::HEARTS).len() as u8,
-                                (state.won[1] & Cards::HEARTS).len() as u8,
-                                (state.won[2] & Cards::HEARTS).len() as u8,
-                                (state.won[3] & Cards::HEARTS).len() as u8,
-                            ],
-                            queen_winner: winner_of(&state, Card::QueenSpades),
-                            ten_winner: winner_of(&state, Card::TenClubs),
-                            jack_winner: winner_of(&state, Card::JackDiamonds),
-                        });
-                    }
-                }
+            })
+        })?;
+        Ok(warp::reply::json(&games))
+    }
+
+    warp::path!("scores")
+        .and(db)
+        .and(warp::query())
+        .and_then(handle)
+}
+
+fn read_games(mut rows: Rows<'_>) -> Result<Vec<CompleteGame>, rusqlite::Error> {
+    let mut games = Vec::new();
+    let mut hands = Vec::with_capacity(4);
+    let mut state = GameState::new();
+    while let Some(row) = rows.next()? {
+        let game_id = row.get(0)?;
+        let timestamp = row.get(1)?;
+        let event = row.get(2)?;
+        let was_playing = state.phase.is_playing();
+        state.apply(&event);
+        let is_playing = state.phase.is_playing();
+        if was_playing && !is_playing {
+            hands.push(CompleteHand {
+                charges: state.charged,
+                hearts_won: [
+                    (state.won[0] & Cards::HEARTS).len() as u8,
+                    (state.won[1] & Cards::HEARTS).len() as u8,
+                    (state.won[2] & Cards::HEARTS).len() as u8,
+                    (state.won[3] & Cards::HEARTS).len() as u8,
+                ],
+                queen_winner: winner_of(&state, Card::QueenSpades),
+                ten_winner: winner_of(&state, Card::TenClubs),
+                jack_winner: winner_of(&state, Card::JackDiamonds),
+            });
+            if hands.len() == 4 {
+                let mut complete_hands = Vec::new();
+                mem::swap(&mut hands, &mut complete_hands);
                 games.push(CompleteGame {
                     game_id,
                     completed_time: timestamp,
@@ -128,26 +146,13 @@ pub fn scores(db: infallible!(Database)) -> reply!() {
                             user_id: state.players[3],
                         },
                     ],
-                    hands,
+                    hands: complete_hands,
                 });
+                state = GameState::new();
             }
-            Ok(games)
-        })?;
-        Ok(warp::reply::json(&games))
+        }
     }
-
-    warp::path!("scores")
-        .and(db)
-        .and(warp::query())
-        .and_then(handle)
-}
-
-fn read_game_ids(mut rows: Rows<'_>) -> Result<Vec<GameId>, rusqlite::Error> {
-    let mut game_ids = Vec::new();
-    while let Some(row) = rows.next()? {
-        game_ids.push(row.get(0)?);
-    }
-    Ok(game_ids)
+    Ok(games)
 }
 
 fn winner_of(state: &GameState, card: Card) -> UserId {
