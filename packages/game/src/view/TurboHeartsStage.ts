59527ca2b7b7f0870d9b56f5f0f7355c776387f1
Brian Lee <blee@palantir.com>
2020-03-12 12:07:11 -0700

    Revert "Don't allow already charged cards to be charged again."
    This reverts commit 239849de8b06d315ad723a80e9c6cd74fd8e5fb2.

--- packages/game/src/view/TurboHeartsStage.ts
+++ packages/game/src/view/TurboHeartsStage.ts
@@ -210,7 +210,7 @@ export class TurboHeartsStage {
     if (player.action === "pass") {
       legalPlays = player.hand;
     } else if (player.action === "charge") {
-      legalPlays = CHARGEABLE_CARDS.filter(card => !player.charged.includes(card))
+      legalPlays = CHARGEABLE_CARDS;
     } else if (player.action === "play") {
       legalPlays = player.legalPlays;
     }
