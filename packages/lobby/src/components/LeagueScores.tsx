3f7b10280d9a5abd8cd0863cf1d6e67fd4f6add1
Brian Lee <blee@palantir.com>
2020-03-18 08:37:27 -0700

    Frontend leaderboard table sorting.

--- packages/lobby/src/components/LeagueScores.tsx
+++ packages/lobby/src/components/LeagueScores.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import { connect } from "react-redux";
 import { LobbyState, UsersState } from "../state/types";
 import { GameResult } from "../types";
-import { calculateScores, GameScores, calculateLeaderboard } from "../utils/gameResults";
+import { calculateScores, GameScores, calculateLeaderboard, LeaderboardEntry } from "../utils/gameResults";
+
+type Sort = "user" | "points" | "games" | "ppg";
 
 export namespace LeagueScores {
     export interface OwnProps {}
@@ -15,6 +17,11 @@ export namespace LeagueScores {
     export interface DispatchProps {}
 
     export type Props = OwnProps & StoreProps & DispatchProps;
+
+    export interface State {
+        sort: Sort;
+        reverse: boolean;
+    }
 }
 
 function mapStateToProps(state: LobbyState): LeagueScores.StoreProps {
@@ -24,7 +31,12 @@ function mapStateToProps(state: LobbyState): LeagueScores.StoreProps {
     };
 }
 
-class LeagueScoresInternal extends React.PureComponent<LeagueScores.Props> {
+class LeagueScoresInternal extends React.PureComponent<LeagueScores.Props, LeagueScores.State> {
+    public state: LeagueScores.State = {
+        sort: "points",
+        reverse: false
+    };
+
     public render() {
         const allScores = this.props.games.map(calculateScores);
         return (
@@ -37,16 +49,18 @@ class LeagueScoresInternal extends React.PureComponent<LeagueScores.Props> {
 
     private renderLeaderboard(allScores: GameScores[]) {
         const leaderboard = calculateLeaderboard(allScores);
-        leaderboard.sort((a, b) => b.points - a.points);
+        const reverse = this.state.reverse ? -1 : 1;
+        leaderboard.sort((a, b) => this.getSortFunction()(a, b) * reverse);
         return (
             <div>
                 <h3>Leaderboard</h3>
                 <table>
                     <thead>
                         <tr>
-                            <th>User</th>
-                            <th>Points</th>
-                            <th>Games</th>
+                            <th onClick={this.handleSortRequest("user")}>User</th>
+                            <th onClick={this.handleSortRequest("points")}>Points</th>
+                            <th onClick={this.handleSortRequest("games")}>Games</th>
+                            <th onClick={this.handleSortRequest("ppg")}>PPG</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -55,12 +69,47 @@ class LeagueScoresInternal extends React.PureComponent<LeagueScores.Props> {
                                 <td>{this.renderUserName(entry.userId)}</td>
                                 <td>{entry.points}</td>
                                 <td>{entry.games}</td>
+                                <td>{Math.round(entry.points / entry.games)}</td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
             </div>
         );
+    }
+
+    private handleSortRequest = (sort: Sort) => () => {
+        if (this.state.sort === sort) {
+            this.setState({
+                sort: this.state.sort,
+                reverse: !this.state.reverse
+            })
+        } else {
+            this.setState({
+                sort,
+                reverse: false
+            })
+        }
+    }
+
+    private getSortFunction(): (a: LeaderboardEntry, b: LeaderboardEntry) => number {
+        switch (this.state.sort) {
+            case "points":
+                return (a, b) => b.points - a.points;
+            case "games":
+                return (a, b) => b.games - a.games;
+            case "ppg":
+                return (a, b) => b.points / b.games - a.points / a.games;
+            case "user":
+                return (a, b) => {
+                    const userA = this.renderUserName(a.userId);
+                    const userB = this.renderUserName(b.userId);
+                    if (userA == null || userB == null) {
+                        return 0;
+                    }
+                    return userA.localeCompare(userB);
+                };
+        }
     }
 
     private renderUserName(userId: string | undefined) {
